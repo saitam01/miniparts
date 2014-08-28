89bfa70bce9e5d4bbf30a9c97c1af5cff9e15d95
maguirre <maguirre@kioslutions.com>
2014-08-28 16:04:04 -0400

    actualizacion javascript

--- src/main/webapp/resources/js/mainpage/main-page.js
+++ src/main/webapp/resources/js/mainpage/main-page.js
@@ -7,38 +7,36 @@ var fila = '';
 
 $(document).ready(function(){
 
+    var subMenuDrawer = function (subMenuVOList) {
+        var subFila = $('<div></div>');
+        $.each(subMenuVOList,
+            function () {
+                subFila.append($('<div class="panel-body"></div>').html(this.submenu));
+            }
+        );
+        return subFila;
+    };
+
     $.post('get-menu', '', function(data){
         var level1 = 0;
         fila = $('<div></div>');
         $.each(data.menuVOList, function (){
-
             /**Menu*/
             fila.append($('<div class="panel panel-default"></div>')
                 .html($('<div class="panel-heading"></div>')
                     .html($('<h4 class="panel-title"></h4>')
                         .html($('<a data-toggle="collapse" data-parent="#accordionChild" href="#collapseChild_' + level1 + '"></a>"')
                             .html(this.nombreMenu)
-                                /*.html($('<div id="collapseChild_' + level1 + '" class="panel-collapse collapse"></div>')
-                                    .html(function () {
-                                            $.each(data.subMenuVOList, function () {
-                                                $('<div class="panel-body"></div>').html(this.submenu);
-                                            });
-                                        }
-                                )
-                            )*/
                         )
                     )
                 )
-                .append($('<div id="collapseChild_' + level1 + '" class="panel-collapse collapse"></div>')
-                    .html(function () {
-                            $.each(this.subMenuVOList,
-                                function () {
-                                    $('<div class="panel-body"></div>').html(this.submenu);
-                                });
-                            }
-                    )
-                )
             );
+
+
+            fila.append($('<div id="collapseChild_' + level1 + '" class="panel-collapse collapse"></div>')
+                    .html($(subMenuDrawer(this.subMenuVOList)))
+            );
+
             level1 = level1 + 1;
         });
 
